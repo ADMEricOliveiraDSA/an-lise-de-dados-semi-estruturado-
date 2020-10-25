0302a3ea87581e1638faeed9ed9150be606a6751
Eric Oliveira <ericoliveira1717@gmail.com>
2020-10-25 19:37:15 -0300

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-@app.route('/')
-def index ():
-    return 'Aplicação Web com Python e Flask! Por ERIC OLIVEIRA DATA SCIENTIST'
-      
-app.run(host='0.0.0.0',port=81)
